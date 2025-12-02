f9ffe912820fc0ab468a5af8e167f1b148804660
cookie1170 <171882521+cookie1170@users.noreply.github.com>
2025-12-03 07:33:42 +1000

    use a span instead of strings // ~600ms -> ~530ms

--- Day02/Day02Solution.cs
+++ Day02/Day02Solution.cs
@@ -7,6 +7,7 @@ namespace Day02;
  * Second attempt: 1000ms
  * Third attempt: 700ms
  * Fourth attempt: 600ms
+ * Fifth attempt: 530ms
  */
 
 public class Day02Solution : DaySolution
@@ -29,29 +30,24 @@ public class Day02Solution : DaySolution
     }
 
     private static bool IsInvalid(long id) {
-        string asString = id.ToString();
-        int length = asString.Length;
+        Span<char> buffer = stackalloc char[20];
+        id.TryFormat(buffer, out int length);
         
         for (int i = 1; i <= length / 2; i++) {
             if (length % i != 0) continue;
             
-            if (AllSame(length, i, asString)) return true;
+            if (AllSame(length, i, buffer)) return true;
         }
 
         return false;
     }
 
-    private static bool AllSame(int length, int i, string asString) {
-        string? first = null;
-        for (int j = 0; j <= length - i; j += i) {
-            string substring = asString.Substring(j, i);
-            if (first == null) {
-                first = substring;
-
-                continue;
-            }
+    private static bool AllSame(int length, int i, Span<char> span) {
+        Span<char> first = span[..i];
+        for (int j = i; j <= length - i; j += i) {
+            Span<char> substring = span.Slice(j, i);
                 
-            if (substring != first) return false;
+            if (!substring.SequenceEqual(first)) return false;
         }
 
         return true;
